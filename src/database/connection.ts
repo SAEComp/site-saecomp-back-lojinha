143207de1acb4a59a35fb241b8764cacddc66c8a
XandGVaz <vitorgarciavaz@gmail.com>
2025-08-14 17:10:55 -0300

    fix: connection with database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,15 +1,15 @@
 import {Pool} from 'pg';
 
-// Vem dom servidor postgresSQL
+// Conexão com database servidor postgresSQL
 const poolConfig = {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-    ssl: false,
-    max: 6
-}
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT ?? 5433),
+  user: process.env.DB_USER ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'postgres',
+  database: process.env.DB_NAME ?? 'saecomp',
+  ssl: false,
+  max: 6
+};
 
 // Gerencia a "piscina de conexões" do banco de dados
 //  - Reutiliza conexões
