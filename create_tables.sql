b223a6147ba13e04024639e80bd44b5b476287ac
XandGVaz <vitorgarciavaz@gmail.com>
2025-08-14 17:51:39 -0300

    add: create produc tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,18 @@
+CREATE TYPE category AS ENUM(sweet, salty, drink);
+
 CREATE TABLE IF NOT EXISTS produto(
-	id		BIGSERIAL 	PRIMARY KEY,
-	name 	TEXT 		NOT NULL
+	id				BIGSERIAL 	PRIMARY KEY,
+	name 			TEXT 		NOT NULL,
+	value			REAL		NOT NULL,
+	description		TEXT		NOT NULL,
+	quantity		INTEGER		CHECK(quantity >= 0),
+	barCode			VCHAR(13)	UNIQUE ,
+	softDelete		BOOLEAN		DEFAULT FALSE
 );
+
+CREATE TABLE IF NOT EXISTS historyProduct(
+	id				BIGSERIAL	PRIMARY KEY,
+	entryDate		DATE 		NOT NULL DEFAULT CURRENT_DATE,
+	entryquantity	INTEGER		CHECK(quantity >= 0)
+);
+
