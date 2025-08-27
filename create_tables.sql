a77c34824544f3687b566e244fa5c667bb53d25d
XandGVaz <vitorgarciavaz@gmail.com>
2025-08-27 18:31:37 -0300

    fix: unicidade do carrinho para cada usuário de um produto para cada carrinho

--- create_tables.sql
+++ create_tables.sql
@@ -1,15 +1,22 @@
-CREATE TYPE category_t AS ENUM('sweet', 'salty', 'drink');
-CREATE TYPE status_t AS ENUM('cart', 'pendingPayment', 'finishedPayment');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'category_t') THEN
+        CREATE TYPE category_t AS ENUM('sweet', 'salty', 'drink');
+    END IF;
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'status_t') THEN
+        CREATE TYPE status_t AS ENUM('cart', 'pendingPayment', 'finishedPayment');
+    END IF;
+END$$;
 
-create table if not exists users (
-    id serial primary key,
-    google_sub text not null unique,
-    name text,
-    email text not null,
-    role text check (role in ('admin', 'user')) default 'user',
-    nusp varchar(10) unique,
-    created_at timestamp with time zone default now()
-);
+-- create table if not exists users (
+--     id serial primary key,
+--     google_sub text not null unique,
+--     name text,
+--     email text not null,
+--     role text check (role in ('admin', 'user')) default 'user',
+--     nusp varchar(10) unique,
+--     created_at timestamp with time zone default now()
+-- );
 
 CREATE TABLE IF NOT EXISTS products(
 	id				BIGSERIAL 		PRIMARY KEY,
@@ -30,17 +37,19 @@ CREATE TABLE IF NOT EXISTS entry_histories(
 );
 
 CREATE TABLE IF NOT EXISTS buy_orders(
-	id				BIGSERIAL		PRIMARY KEY,
-	users_id		INT 			NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-	date			DATE 			NOT NULL DEFAULT CURRENT_DATE,
-	status			status_t		NOT NULL DEFAULT 'cart'
+	id				BIGSERIAL			PRIMARY KEY,
+	users_id		INT 				NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	date			DATE 				NOT NULL DEFAULT CURRENT_DATE,
+	status			status_t			NOT NULL DEFAULT 'cart',
+	CONSTRAINT 		unique_user_cart 	UNIQUE (users_id, status)
 );
 
 CREATE TABLE IF NOT EXISTS items(
-	id				BIGSERIAL		PRIMARY KEY,
-	products_id		INT				NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-	buy_orders_id	INT 			NOT NULL REFERENCES buy_orders(id) ON DELETE CASCADE,
-	quantity		INTEGER			CHECK(quantity >= 0)
+	id				BIGSERIAL			PRIMARY KEY,
+	products_id		INT					NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+	buy_orders_id	INT 				NOT NULL REFERENCES buy_orders(id) ON DELETE CASCADE,
+	quantity		INTEGER				CHECK(quantity >= 0),
+	CONSTRAINT 		unique_cart_product UNIQUE (buy_orders_id, products_id)
 );
 
 CREATE TABLE IF NOT EXISTS comments(
--- src/repositories/client/addToCartData.repository.ts
+++ src/repositories/client/addToCartData.repository.ts
@@ -9,26 +9,28 @@ const dbQueryVerifyProduct = `
 `;
 
 const dbQuerySearchCart = `
-    SELECT 1 FROM buy_orders
-    WHEN buy_orders.users_id = $1
+    SELECT id FROM buy_orders
+    WHERE buy_orders.users_id = $1
         AND buy_orders.status = 'cart'
 `;
 
 const dbQueryCreateCart = `
     INSERT INTO buy_orders (users_id, date, status)
     VALUES ($1, CURRENT_DATE, 'cart')
+    ON CONFLICT (users_id, status) DO NOTHING
     RETURNING id
 `;
 
 const dbQuerySearchItem = `
-    SELECT 1 FROM items
-    WHERE items.buy_orders_id = $1
+    SELECT id FROM items
+    WHERE buy_orders_id = $1
         AND products_id = $2
 `;
 
 const dbQueryCreateItem = `
     INSERT INTO items (products_id, buy_orders_id, quantity)
     VALUES ($1, $2, $3)
+    ON CONFLICT (buy_orders_id, products_id) DO NOTHING
 `;
 
 const dbQueryUpdateItems = `
@@ -42,49 +44,65 @@ const dbQueryUpdateItems = `
 `;
 
 const addtoCartData = async(user_id: number, product_id: number, quantity: number): Promise<number|null> => {
-
+    
+    // Valor retornado pela função 
     var returned: number|null= 0;
+    
+    // Variáveis de checagem de atualização do carrinho
     var checkProduct: number|null|undefined = 0;
     var cart_id: number|null|undefined = 0;
     var item_id: number|null|undefined = 0;
     var qntItensUpdated : number|null|undefined = 0;
 
-    // Tenta criar ordem de compra, caso ela já exista para o usuário, 
+    // Obtenção de conexão exclusiva com BD
     const client  = await pool.connect();
+    
+    // Processo de atualização da ordem de compra
     try{
+
+        // Início de transação com BD
         await client.query('BEGIN');
         
+        // Checagem de existência do produto e se está disponível em quantidade suficiente 
         checkProduct = (await client.query(dbQueryVerifyProduct, [product_id, quantity])).rowCount;
 
+        // Se disponível
         if(checkProduct){
 
+            // Checagem de existência do carrinho, e, caso não exista, é criado
             cart_id = (await client.query(dbQuerySearchCart, [user_id])).rows[0]?.id;
-
             if(!cart_id){
                 cart_id = (await client.query(dbQueryCreateCart, [user_id])).rows[0]?.id;
             }
             
+            /* 
+                Verificação da existência do item que conecta carrinho ao produto, se existir é atualizado
+                e, caso não exista, é criado
+            */
             item_id = (await client.query(dbQuerySearchItem, [cart_id, product_id])).rows[0]?.id;
             if(item_id){
                 qntItensUpdated = (await client.query(dbQueryUpdateItems, [quantity, user_id, product_id])).rowCount;
-                returned = 1;
+                returned = qntItensUpdated;
             }
             else{
                 await client.query(dbQueryCreateItem, [product_id, cart_id, quantity]);
                 returned = 1;
             }
-
-
         }
 
+        // Finaliza transação com BD
         await client.query('COMMIT');
 
     } catch (error){
 
+        // Caso ocorra algum erro, querys são revertidas
         await client.query('ROLLBACK');
+        returned = -1
         throw error;
 
     } finally {
+
+        // Termina conexão exclusiva com BD
         client.release();
     }
 
