d481c9cc35bf5dfe5855c4d8e5726987b9b5bdf9
XandGVaz <vitorgarciavaz@gmail.com>
2025-08-27 23:16:08 -0300

    dfinição das pastas de schemas

--- src/controllers/client/getProductPage.controller.ts
+++ src/controllers/client/getProductPage.controller.ts
@@ -1,2 +1,8 @@
 import { Request, Response} from "express";
 import { Product } from "../../interfaces/product.interface";
+import getProductPageData from "../../repositories/client/getProductPageData.repository";
+
+
+const getProductPage = async(req: Request, res: Response): Promise<void> => {
+    
+}
