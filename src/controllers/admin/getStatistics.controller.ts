9746b888814f057b0dcb7b9a5865ffda28fe3f54
XandGVaz <vitorgarciavaz@gmail.com>
2025-09-08 14:10:27 -0300

    fieature: funnção de repositório para obtenção de estatísticas

--- src/controllers/admin/getStatistics.controller.ts
+++ src/controllers/admin/getStatistics.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response} from "express";
 import { ApiError } from "../../errors/ApiError";
-import { getStatisticsOutSchema, ICGetStatisticsOutSchema } from "../../schemas/lojinha/output/getStatisticsOut.schema";
+import { getStatisticsInSchema, ICGetStatisticsInSchema } from "../../schemas/lojinha/input/getStatisticsIn.schema";
 import { getStatisticsData } from "../../repositories/admin/getStatisticsData.repository";
 
 const getStatistics = async(req: Request, res: Response): Promise<void> => {
 
+    const query = getStatisticsInSchema.parse(req.query);
+
+    const result = getStatisticsData(query);
+
+    res.json(result);
 }
 
 export default getStatistics;
--- src/repositories/admin/getStatisticsData.repository.ts
+++ src/repositories/admin/getStatisticsData.repository.ts
@@ -1,8 +1,94 @@
 import pool from "../../database/connection";
 import { ApiError } from "../../errors/ApiError";
+import { ICGetStatisticsInSchema } from "../../schemas/lojinha/input/getStatisticsIn.schema";
 import { ICGetStatisticsOutSchema } from "../../schemas/lojinha/output/getStatisticsOut.schema";
+import { BuyOrder } from "../../interfaces/order.interface";
+import { Item } from "../../interfaces/Item.interface";
 
-export const getStatisticsData = async(): Promise<ICGetStatisticsOutSchema> => {
-    // TODO: Implement actual logic to fetch statistics data
+
+const dbQueryGetOrderStats = `
+    SELECT 
+        COUNT(id) FILTER (WHERE status!='cart') AS "totalOrders",
+        COUNT(id) FILTER (WHERE status = 'pendingPayment') AS "pendingOrders",
+        COUNT(id) FILTER (WHERE status = 'finishedPayment') AS "finishedOrders",
+        COUNT(id) FILTER (WHERE status = 'canceled') AS "canceledOrders"
+    FROM buy_orders
+`;
+
+const dbQueryGetProductsStats= `
+    SELECT 
+        COUNT(id) FILTER (WHERE soft_delete = false) AS "stockProducts",
+        SUM(products.value * products.quantity) AS "stockItems" 
+    FROM products
+`;
+
+const dbQueryGetRevenue = `
+    SELECT 
+        SUM(i.quantity * i.value) AS "totalRevenueValue"
+    FROM items i
+    INNER JOIN buy_orders bo ON i.buy_order_id = bo.id
+    WHERE
+        bo.status = 'finishedPayment'
+`;
+
+const dbQueryProductsWithMoreRevenue = `
+    SELECT
+        p.id AS "id",
+        p.name AS "name",
+        SUM(i.quantity) AS "soldQuantity",
+        SUM(i.quantity * i.value) AS "revenue"
+    FROM products p
+    INNER JOIN items i ON i.product_id = p.id
+    WHERE p.soft_delete = false
+    GROUP BY p.id, p.name
+    ORDER BY SUM(i.quantity * i.value) DESC
+`;
+
+const dbQueryProductsWithMoreSolds = `
+    SELECT
+        p.id AS "id",
+        p.name AS "name",
+        SUM(i.quantity) AS "soldQuantity",
+        SUM(i.quantity * i.value) AS "revenue"
+    FROM products p
+    INNER JOIN items i ON i.product_id = p.id
+    WHERE p.soft_delete = false
+    GROUP BY p.id, p.name
+    ORDER BY SUM(i.quantity) DESC
+`;
+
+
+export const getStatisticsData = async(statisticsInfo: ICGetStatisticsInSchema): Promise<ICGetStatisticsOutSchema> => {
+
+    const { lessSoldQnt, moreSoldQnt} = statisticsInfo;
+
+    let result : ICGetStatisticsOutSchema; 
+
+    const client = await pool.connect();
+
+    try{
+        await client.query('BEGIN');
+
+        result = (await client.query(dbQueryGetProductsStats)).rows[0];
+
+        result = (await client.query(dbQueryGetProductsStats)).rows[0];
+
+        result = (await client.query(dbQueryGetRevenue)).rows[0];
+
+        result.productWithMoreRevenueValue = (await client.query(dbQueryProductsWithMoreRevenue)).rows[0];
+
+        result.productWithMoreSoldQuantity = (await client.query(dbQueryProductsWithMoreSolds)).rows[0];
+        
+        await client.query('COMMIT');
+    }
+    catch(error){
+        await client.query('ROLLBACK');
+        throw error;
+    }
+    finally{
+        client.release();
+    }
+
+
     return {} as ICGetStatisticsOutSchema;
 }
