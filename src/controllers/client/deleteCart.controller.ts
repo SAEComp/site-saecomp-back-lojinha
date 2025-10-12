e39912fc2ff88f2b089aa6f142c2ab1691687993
XandGVaz <vitorgarciavaz@gmail.com>
2025-10-12 20:50:44 -0300

    fix: lançamento de erros de exclusão de carrinho na própria função de repository

--- src/controllers/client/deleteCart.controller.ts
+++ src/controllers/client/deleteCart.controller.ts
@@ -8,13 +8,10 @@ const deleteCart = async(req: Request, res: Response): Promise<void> =>{
     const userId = req.userId;
     
     // Verificação se id do usuário está disponível
-    if(userId === undefined) throw new ApiError(404, 'Usuário não encontrado');
+    if(!userId) throw new ApiError(404, 'Usuário não encontrado');
     
-    // Busca carrinho pelo status('cart') e id do usuário
-    const qntDeleted = await deleteCartData(userId);
-    
-    // Verificação se carrinho foi encontrado
-    if(!qntDeleted) throw new ApiError(404, 'Erro ao deletar o carrinho ou carrinho inexistente'); 
+    // Deleta atual carrinho do usuário
+    await deleteCartData(userId);
     
     // Retorno do pedido
     res.status(200).json({message: 'Carrinho deletado com sucesso'});
