81718f1344db8325c1ce674350bfb7602735811d
XandGVaz <vitorgarciavaz@gmail.com>
2026-01-22 10:57:40 -0300

    fix: adequação para suporte a tamanho de imagens

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,9 @@ const app = express();
 const port: number = 3000;
 
 // ================= middlewares ================= //
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(corsMiddleware);
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true, limit: '10mb'}));
 
 // ================= routers ================= //
 app.use('/api/lojinha/health', healthRouter);
